e1e9bca57bf8f56b3fa6971d241150027f3d05a2
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2024-11-21 09:52:22 -0700

    specific output for consistency

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,5 +6,5 @@ output "pingone_environment_information" {
 }
 
 output "mfa_device_policies" {
-  value = data.pingone_mfa_device_policies.mfa_device_policies
+  value = data.pingone_mfa_device_policies.mfa_device_policies.ids
 }
