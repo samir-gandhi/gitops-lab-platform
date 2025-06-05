c26fdb693984ec0682a7a2230ecd93bcad3628f4
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2025-06-05 17:42:00 -0600

    formatting

--- 01-infrastructure/helm_ping_devops.tf
+++ 01-infrastructure/helm_ping_devops.tf
@@ -4,10 +4,10 @@
 # }
 
 resource "helm_release" "ping_devops" {
-  name            = var.k8s_helm_deployment_name
-  namespace       = kubernetes_namespace_v1.gitops-lab.metadata[0].name
+  name      = var.k8s_helm_deployment_name
+  namespace = kubernetes_namespace_v1.gitops-lab.metadata[0].name
   # cleanup_on_fail = var.k8s_helm_deployment_name == "prod" || var.k8s_helm_deployment_name == "qa" ? false : true
-  timeout         = 360
+  timeout = 360
 
   repository = "https://helm.pingidentity.com"
   chart      = "ping-devops"
