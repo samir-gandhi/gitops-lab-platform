dc7a4b583cad998a09bb9affb3609fc6c60fb8d3
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2025-06-06 00:22:52 -0600

    add pd data store

--- 02-configuration/pf_data_store.tf
+++ 02-configuration/pf_data_store.tf
@@ -80,3 +80,45 @@
 #     mask_attribute_values = false
 #   }
 # }
+
+# # __generated__ by Terraform from "LDAP-D803C87FAB2ADFB4B0A947B64BA6F0C6093A5CA3"
+# resource "pingfederate_data_store" "pingcli__LDAP-D803C87FAB2ADFB4B0A947B64BA6F0C6093A5CA3_LDAP" {
+#   data_store_id     = "LDAP-D803C87FAB2ADFB4B0A947B64BA6F0C6093A5CA3"
+#   ldap_data_store = {
+#     bind_anonymously           = false
+#     connection_timeout         = 3000
+#     create_if_necessary        = true
+#     dns_ttl                    = 60000
+#     # TODO: convert this to a regular password
+#     # encrypted_password         = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiRW1JY1UxOVdueSIsInZlcnNpb24iOiIxMi4yLjIuMCJ9..tqwu5cDSgLvJ1S775baMLQ.kHMAz46_lCYMpWeqV-_n9w.yiRzDgTv7BgCTnDr4loB3g"
+#     password = data.terraform_remote_state.infrastructure.outputs.pingfederate_api_password
+#     follow_ldap_referrals      = false
+#     hostnames                  = ["${var.pingone_environment_name}-pingdirectory:389"]
+#     hostnames_tags = [
+#       {
+#         default_source = true
+#         hostnames      = ["${var.pingone_environment_name}-pingdirectory:389"]
+#         tags           = null
+#       },
+#     ]
+#     ldap_dns_srv_prefix     = "_ldap._tcp"
+#     ldap_type               = "PING_DIRECTORY"
+#     ldaps_dns_srv_prefix    = "_ldaps._tcp"
+#     max_connections         = 100
+#     max_wait                = -1
+#     min_connections         = 10
+#     name                    = "pingdirectory"
+#     read_timeout            = 3000
+#     retry_failed_operations = false
+#     test_on_borrow          = false
+#     test_on_return          = false
+#     time_between_evictions  = 60000
+#     use_dns_srv_records     = false
+#     use_ssl                 = false
+#     use_start_tls           = false
+#     user_dn                 = "cn=pingfederate"
+#     verify_host             = true
+#   }
+#   mask_attribute_values            = false
+#   ping_one_ldap_gateway_data_store = null
+# }
