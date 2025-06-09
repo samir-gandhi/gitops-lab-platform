8a9b807bc0ba8f05e4f88d474abec465e35918d0
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2025-06-09 16:53:26 -0600

    fmt

--- 02-configuration/davinci_flow.tf
+++ 02-configuration/davinci_flow.tf
@@ -19,7 +19,7 @@ resource "davinci_flow" "pingone_davinci_api_protect_example" {
   flow_json = file("./davinci_flows/davinci-api-protect-reg-authn-flow.json")
 
   log_level = 2
-  
+
   deploy = true
 
 
