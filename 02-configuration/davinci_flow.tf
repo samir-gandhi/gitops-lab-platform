fe156985b279c215c7be5e5bb4d7c0f11190b053
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2025-06-09 16:37:44 -0600

    latest dv version

--- 02-configuration/davinci_flow.tf
+++ 02-configuration/davinci_flow.tf
@@ -18,7 +18,7 @@ resource "davinci_flow" "pingone_davinci_api_protect_example" {
 
   flow_json = file("./davinci_flows/davinci-api-protect-reg-authn-flow.json")
 
-  log_level = 2
+  log_level = 1
 
 
   // Connector link: httpConnector
