262a51fa57011e3e0866ec47d21832ab8e002b60
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2025-06-09 16:23:25 -0600

    force log_level

--- 02-configuration/davinci_flow.tf
+++ 02-configuration/davinci_flow.tf
@@ -18,6 +18,8 @@ resource "davinci_flow" "pingone_davinci_api_protect_example" {
 
   flow_json = file("./davinci_flows/davinci-api-protect-reg-authn-flow.json")
 
+  log_level = 2
+
 
   // Connector link: httpConnector
   connection_link {
