dbd1cb1347953d9d88d067ef664a94f931fb2d56
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2026-02-03 20:33:14 -0700

    initial export

--- configuration/ping-export-module.tf
+++ configuration/ping-export-module.tf
@@ -4,13 +4,13 @@ module "ping-export" {
   pingone_environment_id = var.pingone_environment_id
 
   # Connection Variables
-  davinci_connection_PingOne-0020-Protect_clientId = var.davinci_connection_PingOne-0020-Protect_clientId
+  davinci_connection_PingOne-0020-Protect_clientId     = var.davinci_connection_PingOne-0020-Protect_clientId
   davinci_connection_PingOne-0020-Protect_clientSecret = var.davinci_connection_PingOne-0020-Protect_clientSecret
-  davinci_connection_PingOne-0020-Protect_envId = var.davinci_connection_PingOne-0020-Protect_envId
-  davinci_connection_PingOne-0020-Protect_region = var.davinci_connection_PingOne-0020-Protect_region
-  davinci_connection_PingOne_clientId = var.davinci_connection_PingOne_clientId
-  davinci_connection_PingOne_clientSecret = var.davinci_connection_PingOne_clientSecret
-  davinci_connection_PingOne_envId = var.davinci_connection_PingOne_envId
-  davinci_connection_PingOne_region = var.davinci_connection_PingOne_region
+  davinci_connection_PingOne-0020-Protect_envId        = var.davinci_connection_PingOne-0020-Protect_envId
+  davinci_connection_PingOne-0020-Protect_region       = var.davinci_connection_PingOne-0020-Protect_region
+  davinci_connection_PingOne_clientId                  = var.davinci_connection_PingOne_clientId
+  davinci_connection_PingOne_clientSecret              = var.davinci_connection_PingOne_clientSecret
+  davinci_connection_PingOne_envId                     = var.davinci_connection_PingOne_envId
+  davinci_connection_PingOne_region                    = var.davinci_connection_PingOne_region
 
 }
--- configuration/ping-export-module/pingone_davinci_application.tf
+++ configuration/ping-export-module/pingone_davinci_application.tf
@@ -2,15 +2,15 @@
 resource "pingone_davinci_application" "pingcli__DaVinci-0020-API-0020-Protect-0020-Sample-0020-Application" {
   environment_id = var.pingone_environment_id
 
-  name           = "DaVinci API Protect Sample Application"
+  name = "DaVinci API Protect Sample Application"
 
   api_key = {
     enabled = true
   }
 
   oauth = {
-    grant_types                   = ["authorizationCode"]
-    redirect_uris                 = ["https://auth.pingone.com/104f7047-128e-4794-8c62-dcd0a7fcf74c/rp/callback/openid_connect"]
-    scopes                        = ["openid", "profile"]
+    grant_types   = ["authorizationCode"]
+    redirect_uris = ["https://auth.pingone.com/104f7047-128e-4794-8c62-dcd0a7fcf74c/rp/callback/openid_connect"]
+    scopes        = ["openid", "profile"]
   }
 }
